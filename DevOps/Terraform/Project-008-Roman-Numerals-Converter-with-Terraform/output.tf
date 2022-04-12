fa9a03bd7c0bc68bc371663a69664176cff27765
engin777 <engkzr777@gmail.com>
2022-04-13 00:39:55 +0300

    Project-8 added

---
+++ DevOps/Terraform/Project-008-Roman-Numerals-Converter-with-Terraform/output.tf
@@ -0,0 +1,3 @@
+output "aws_public_ip" {
+  value = aws_instance.Roman_Numerals.public_ip
+}
---
+++ DevOps/Terraform/Project-008-Roman-Numerals-Converter-with-Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+  default = "vpc-0cf9fe15602f8ca1c"
+}
+
+variable "keyname" {
+  default = "xxxxxx"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
